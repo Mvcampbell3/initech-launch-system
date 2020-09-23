fdb14dca64beb9b8ccb73bde57c6c4f39573690c
Mvcampbell3 <mvcampbell3@gmail.com>
2020-09-22 23:53:20 -0400

    react-router-dom added with landing page and redirect to store page with nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import Nav from './components/Nav'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+// pages
+import Landing from './pages/Landing';
+import Store from './pages/Store';
+
+// components
+
+/*
+
+  We are going to be having the majority of important life-cycle state live inside of App.js, things like
+  user status, loading, error-management, and product holding. The majority of these will be part of a bunch of 
+  useState hooks and useEffect hooks. We are also going to use some useMemo and useRef to hold on to things we need to
+  interact with and not cause re-rendering. It's gonna be fun!
+
+  After we get some more basic front-end stuff done, we will jump into firebase interactions and stripe. When we get there, it might be
+  better to get together or have a zoom call to talk about what we are going to be doing.
+
+*/
 
 function App() {
   return (
-    <div className="app">
-      <Nav />
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' render={props => <Landing {...props} />} />
+        <Route exact path='/store' render={props => <Store {...props} />} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Store/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './store.css';
+import Nav from '../../components/Nav';
+
+const Store = (props) => {
+  return (
+    <div className="store-container">
+      <Nav />
+    </div>
+  );
+}
+
+export default Store;
