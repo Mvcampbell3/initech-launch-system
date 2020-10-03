e7fd57b668792bb61d2843c6b3d508b294ea8f7a
Mvcampbell3 <mvcampbell3@gmail.com>
2020-10-03 01:16:49 -0400

    validation on product create

--- src/pages/ProductInput/index.js
+++ src/pages/ProductInput/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './productInput.css';
-
+import ErrorDisplay from '../../components/ErrorDisplay';
 // Material UI Stuff
 import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -8,7 +8,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-
+import Button from '@material-ui/core/Button'
 import InputGroup from '../../components/InputGroup';
 import TextArea from '../../components/TextArea';
 
@@ -23,23 +23,60 @@ const ProductInput = (props) => {
       marginTop: theme.spacing(2),
     },
   }));
+
   const classes = useStyles();
   const [productType, setProductType] = useState('');
   const [flightProfile, setFlightProfile] = useState('');
   const [missionType, setMissionType] = useState('');
   const [productName, setProductName] = useState('');
   const [productDescription, setProductDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [imgPath, setImgPath] = useState('');
 
   const handleSubmit = e => {
     e.preventDefault();
     console.log('form submitted');
+    let ver_prod_type = productType === '' ?
+      { pass: false, msg: 'Please select product type' } : { pass: true, value: productType, place: 'product_type' };
+    let ver_flight_profile = flightProfile === '' ?
+      { pass: false, msg: 'Please select mission flight profile' } : { pass: true, value: flightProfile, place: 'flight_profile' };
+    let ver_mission_type = missionType === '' ?
+      { pass: false, msg: 'Please select mission type' } : { pass: true, value: missionType, place: 'mission_type' };
+    let ver_prod_name = productName === '' ?
+      { pass: false, msg: 'Plase enter product name' } : { pass: true, value: productName, place: 'name' }
+    let ver_prod_desc = productDescription === '' ?
+      { pass: false, msg: 'Please enter a description of the product' } : { pass: true, value: productDescription, place: 'description' };
+    let ver_price = price === '' ?
+      { pass: false, msg: 'Please enter a price for the product' } : { pass: true, value: price, place: 'price' };
+    let ver_img_path = imgPath === '' ?
+      { pass: false, msg: 'Please enter an image path for the product' } : { pass: true, value: imgPath, place: 'img_path' };
+    const verifyArr = [ver_prod_type, ver_flight_profile, ver_mission_type, ver_prod_name, ver_prod_desc, ver_price, ver_img_path];
+    handleVerify(verifyArr)
+  }
+
+  const handleVerify = (verifyArr) => {
+    console.log(verifyArr)
+    const invalid = [...verifyArr].filter(ver => !ver.pass);
+    if (invalid.length > 0) {
+      // send errordisplay msg
+      let error_arr = invalid.map(item => {
+        return { code: 'invalid product input form item', message: item.msg }
+      })
+      console.log(error_arr)
+      props.setErrors(error_arr);
+    } else {
+      let sendObj = {}
+      verifyArr.forEach(item => {
+        sendObj[item.place] = item.value
+      })
+      console.log(sendObj)
+    }
   }
 
   const handleChange = (e, set) => {
     set(e.target.value);
     if (e.target.value === 'rocket' || e.target.value === 'payload') {
       clearBelowProd();
-
     }
   }
 
@@ -51,10 +88,10 @@ const ProductInput = (props) => {
 
   return (
     <div className="pi-container">
+      {props.displayError ? <ErrorDisplay errorMessages={props.errorMessages} clearErrors={props.clearErrors} /> : null}
       <div className="pi-wrapper">
         <div className="pi-top">
           <h1 className='pi-title'>Product Input</h1>
-          <p className="pi-instructions">This is the area where we can input different products into our store. It will first create a product and price in Stripe, pass back those ids for us to create the product in our firebase application.</p>
         </div>
         <form className="pi-form" onSubmit={(e) => handleSubmit(e)}>
 
@@ -130,10 +167,10 @@ const ProductInput = (props) => {
                       onChange={e => handleChange(e, setMissionType)}
                       label='Mission Type'
                     >
-                      <MenuItem value='earth-sat'>Earth Satellites</MenuItem>
-                      <MenuItem value='planetary-sat'>Beyond Earth Satellites</MenuItem>
-                      <MenuItem value='landers'>Landers</MenuItem>
-                      <MenuItem value='rovers'>Rovers</MenuItem>
+                      <MenuItem value='sat'>Satellite</MenuItem>
+                      <MenuItem value='probe'>Probe</MenuItem>
+                      <MenuItem value='lander'>Lander</MenuItem>
+                      <MenuItem value='rover'>Rover</MenuItem>
                     </Select>
                     <FormHelperText>Some important payload text</FormHelperText>
 
@@ -144,18 +181,47 @@ const ProductInput = (props) => {
             }
           </div>
 
+          <div className="text-holder-tri">
+            <div className="text-pi">
+              <InputGroup
+                value={productName}
+                setValue={setProductName}
+                id='product-name'
+                label='Product Name'
+                auto_complete='off'
+                placeholder="Name"
+                type='text'
+                className='pi-text-input'
+              ></InputGroup>
+            </div>
 
-          <div className="text-holder-pi">
-            <InputGroup
-              value={productName}
-              setValue={setProductName}
-              id='product-name'
-              label='Product Name'
-              auto_complete='off'
-              placeholder="Name"
-              type='text'
-              className='pi-text-input'
-            ></InputGroup>
+
+
+            <div className="text-pi">
+              <InputGroup
+                type='number'
+                label='Product Price in millions'
+                value={price}
+                setValue={setPrice}
+                placeholder='$00.00'
+                auto_complete='off'
+                id='product-price'
+              >
+              </InputGroup>
+            </div>
+
+            <div className="text-pi">
+              <InputGroup
+                type='text'
+                label='Image Path'
+                value={imgPath}
+                setValue={setImgPath}
+                placeholder='rocket.jpg'
+                auto_complete='off'
+                id='image-path'
+              >
+              </InputGroup>
+            </div>
           </div>
 
           <div className="text-holder-pi">
@@ -167,6 +233,10 @@ const ProductInput = (props) => {
               placeholder='enter description here...'
               auto_complete='off'
             ></TextArea>
+          </div>
+
+          <div className="text-holder-pi">
+            <Button type='submit' variant='contained' color='primary'>Create</Button>
           </div>
 
 
